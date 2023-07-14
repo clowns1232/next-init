071fdde1b3c9fa334d5953d12d0c440c1b099a10
황태현 <clowns1232@gmail.com>
2023-07-14 11:55:05 +0900

    feat: todo로 라우팅 구현

---
+++ app/todos/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodosList from "./TodoList";
+
+type Props = {};
+
+function Todos({}: Props) {
+  return (
+    <div className="border-4 border-yellow-500 text-2xl text-yellow-400 p-2">
+      This is Todos Page
+    </div>
+  );
+}
+
+export default Todos;
